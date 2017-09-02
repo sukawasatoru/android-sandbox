df066a249ee1121b9cfa9347f0a0109523428da5
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2017-09-02 22:59:35 +0900

    Remove unused file

--- app/proguard-rules.pro
+++
@@ -1,2 +0,0 @@
-# https://github.com/square/okio/issues/60
--dontwarn okio.**
--- app/src/main/java/jp/tinyport/tinytest/core/di/ActivityModule.java
+++
@@ -1,7 +0,0 @@
-package jp.tinyport.tinytest.core.di;
-
-import dagger.Module;
-
-@Module
-public class ActivityModule {
-}
